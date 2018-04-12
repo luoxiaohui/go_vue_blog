ca13f07187e57697041696d6dfb094824406e278
被打劫的强盗 <380226205@qq.com>
2018-04-12 18:49:56 +0800

    删除不必要文件。

--- blog_user/front_end/src/config/api.js
+++
@@ -1,5 +0,0 @@
-var domain = process.env.API_DOMAIN
-var port = process.env.API_PORT
-
-global.articleList = domain + ":" + port + "/articleList"
-global.articleDetail = domain + ":" + port + "/articleDetail"
